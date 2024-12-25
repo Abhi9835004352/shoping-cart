87b501422313b01c739259f58b6584b69c17e545
Abhishek <abhishekforitutor@gmail.com>
2024-12-25 07:34:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+// const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("6769976d2acf94ef5a442cd5")
+    .then((user) => {
+      if (!user) {
+        console.log("No user!");
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://mongodb:mongodb@cluster0.sdrba.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Abhishek",
+          email: "ahishekf@gial.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
